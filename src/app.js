cc335ec134ce3841b0b15132461d803a4ee3fcd0
Arditti93 <alex.arditti@hotmail.com>
2022-09-08 11:45:11 +0100

    finshed app

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const yargs = require("yargs")
+const { sequelize } = require("./db/connection") 
+const { addMovie, listMovies } = require("./movie/functions")
+
+const app = async (yargsObject) => {
+    try { 
+        await sequelize.sync()
+        if (yargsObject.create) {
+            await addMovie ({title: yargsObject.title, actor: yargsObject.actor})
+            console.log(await listMovies())
+        } else if (yargsObject.read) {
+            console.log(await listMovies())
+        } else if (yargsObject.update) {
+            // update
+        } else if (yargsObject.delete) {
+            // delete
+        } else {
+            console.log("incorrect command")
+        }
+    } catch (error) {
+        console.log(error)
+    }
+} 
+
+app(yargs.argv)
---
+++ src/movie/functions.js
@@ -0,0 +1,18 @@
+const Movie = require("./table")
+
+exports.addMovie = async (movieObject) => {
+    try {
+        await Movie.create(movieObject);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.listMovies = async () => {
+    try {
+        return await Movie.findAll()
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
